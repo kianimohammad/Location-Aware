36b12aab1290b1da199a4bac30ac3ab7a1192882
Mohammad Kiani <kianimohammad@outlook.com>
2020-01-10 17:32:29 -0500

    Location Aware show user location, altitude etc

--- Location Aware/ViewController.swift
+++ Location Aware/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var latLabel: UILabel!
+    @IBOutlet weak var longLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        latLabel.text = String(location.coordinate.latitude)
+        longLabel.text = String(location.coordinate.longitude)
+        speedLabel.text = String(location.speed)
+        courseLabel.text = String(location.course)
+        altitudeLabel.text = String(location.altitude)
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+            if let error = error {
+                print(error)
+            } else {
+                if let placemark = placemarks?[0] {
+                    
+                    var address = ""
+                    if placemark.subThoroughfare != nil {
+                        address += placemark.subThoroughfare! + "\n"
+                    }
+                    
+                    if placemark.thoroughfare != nil {
+                        address += placemark.thoroughfare! + "\n"
+                    }
+                    
+                    if placemark.subLocality != nil {
+                        address += placemark.subLocality! + "\n"
+                    }
+                    
+                    if placemark.subAdministrativeArea != nil {
+                        address += placemark.subAdministrativeArea! + "\n"
+                    }
+                    
+                    if placemark.postalCode != nil {
+                        address += placemark.postalCode! + "\n"
+                    }
+                    
+                    if placemark.country != nil {
+                        address += placemark.country!
+                    }
+                    
+                    self.addressLabel.text = address
+                }
+            }
+            
+            
+        }
+    }
+        
+        
 }
 
